03e85a11ba72339dbae4a837107714fa04f086c6
sachinfe <144200537+sachinfe@users.noreply.github.com>
2024-06-23 11:09:38 +0530

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+import Login from './login/login';
+import { getAuth } from "firebase/auth";
+import { app } from './Firebase/Firebase';
+//import NavigationBar from './navigation/navbar';
+import SignUp from './signup/signup';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Blogs from "./pages/Blogs";
+import Contact from "./pages/Contact";
+import NoPage from "./pages/NoPage";
+import NavigationBar from './navigation/navbar';
+
+function App() {
+
+  // Initialize Firebase Authentication and get a reference to the service
+  getAuth(app);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<NavigationBar />}>
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<SignUp />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
